65815ac738331c2d2b032a84e30d805b8cf74999
Joyonta Saha <Joyontasaha8@gmail.com>
2020-02-06 00:38:55 +0600

    second commit

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="app">
+    <!-- <Skills /> -->
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+    </nav>
+    <div style="background-color:red;"><router-view/></div>
+  </div>
+</template>
+
+<script>
+
+import Skills from "./components/Skills.vue";
+
+export default {
+  name: "app",
+  components: {
+    Skills
+  }
+};
+
+</script>
+
+<style>
+/* #app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px; 
+  }*/
+
+
+@import url('https://fonts.googleapis.com/css?family=Montserrat:400,700');
+body {
+  background-color: #EEEEEE;
+  font-family: 'Montserrat', sans-serif;
+  display: grid;
+  grid-template-rows: auto;
+  justify-items: center;
+  padding-top: 50px;
+}
+
+nav {
+  padding: 20px 20px 20px 0;
+}
+
+nav a {
+  padding: 10px;
+  text-decoration: none;
+  background: #fff;
+  border-radius: 3px;
+  color: rgb(0, 110, 255);
+  font-weight: bold;
+  margin-right: 15px;
+}
+
+</style>
---
+++ src/components/Skills.vue
@@ -0,0 +1,138 @@
+<template>
+  <div class="container">
+    <form @submit.prevent="addSkill">
+      <input type="text" placeholder="Enter a skill you have.." v-model="skill">
+      <!-- {{ skill }} -->
+    </form>
+  
+    <!-- {{ name }}
+
+    {{ btnState ? 'Button is enable' : 'Button is disable'}}
+
+     <button @click="changeName" :disabled="btnState">Button</button> -->
+
+     <div class="holder">
+       <ul>
+         <li v-for="(data, index) in skills" :key='index'>{{data.skill}}
+   <i class="fa fa-minus-circle" v-on:click="remove(index)"></i>
+  </li>
+       </ul>
+       <p>These are the skills that you possess.</p>
+
+       <!-- <p v-if="skills.length > 1">More Skills</p>
+       <p v-else-if="skills.length === 1">one Skills</p> -->
+       
+       <!-- <p v-else>No Skills</p> -->
+
+       <!-- <div :class="{ alert: showAlert, 'anotherClass' : showClass}">Hi</div> -->
+              <!-- <div :class="alertObject">Hi</div> -->
+              <!-- <div :style="{ backgroundColor: bgColor, width: bgWidth, height: bgHeight}">Hi</div> -->
+
+     </div>
+  </div> 
+ 
+</template> 
+
+
+
+<script>
+export default {
+  name: 'Skills',
+  props: {
+  },
+  data() {
+    return {
+      skill: '',
+      // name: 'Joyonta',
+      // btnState: false,
+      skills:[
+        {'skill' : 'Vue js'},
+        {'skill' : 'Frontend developer'}
+      ],
+      // alertObject: {
+      //  alert: true
+      // },
+      // bgColor: 'red',
+      // bgWidth: '100%',
+      // bgHeight: '30px'
+      //  showAlert: true,
+      // showClass: true
+      
+    }
+  },
+  methods: {
+    changeName() {
+      this.name = 'Joyont Saha'
+    },
+    addSkill(){
+          this.skills.push({skill:this.skill});
+          this.skill = '';
+      },
+      remove(id) {
+        this.skills.splice(id, 1);
+      }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+/* h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.alert{
+  background-color: yellow;
+  width: 100%;
+  height: 30%;
+}
+.anotherClass{
+  border: 60px solid black; */
+
+
+@import "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"; 
+
+    .holder {
+    background: #fff;
+  }
+
+  ul {
+    margin: 0;
+    padding: 0;
+    list-style-type: none;
+  }
+  
+  ul li {
+    padding: 20px;
+    font-size: 1.3em;
+    background-color: #E0EDF4;
+    border-left: 5px solid #3EB3F6;
+    margin-bottom: 2px;
+    color: #3E5252;
+  }
+
+  p {
+    text-align:center;
+    padding: 30px 0;
+    color: gray;
+  }
+
+  .container {
+    box-shadow: 0px 0px 40px lightgray;
+  }
+  i {
+    float: right;
+    cursor:pointer;
+  
+  }
+</style>
